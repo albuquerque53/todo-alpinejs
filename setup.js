b249765f1a3dea5522a56405b14e05091daf095b
Gabriel Albuquerque <albuquerque.se@proton.me>
2024-01-14 13:18:42 -0300

    refactor: move js data to a separate file

---
+++ setup.js
@@ -0,0 +1,31 @@
+function setup() {
+    return {
+        activities: [],
+        name: '',
+        date: '',
+
+        getCurrentDate() {
+            const currentDate = new Date();
+
+            const currentYear = currentDate.getFullYear();
+            const currentMonth = currentDate.getMonth() + 1;
+            const currentDay = currentDate.getDate();
+            
+            return `${currentYear}-${String(currentMonth).padStart(2, '0')}-${String(currentDay).padStart(2, '0')}`;
+        },
+
+        initValues() {
+            this.date = this.getCurrentDate()
+        },
+
+        createActivity() {
+            this.activities.push({
+                id: Math.random().toString(16).slice(2),
+                did: false,
+                name: this.name,
+                date: this.date
+            })
+        },
+        
+    }
+}
